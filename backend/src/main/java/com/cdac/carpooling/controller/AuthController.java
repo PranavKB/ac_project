9a810b9c58ec30fe9188ff96a9b0a0799451c5de
ransubhechaitrali <ransubhechaitrali@gmail.com>
2026-06-23 23:55:27 +0530

    auth controller update

--- backend/src/main/java/com/cdac/carpooling/controller/AuthController.java
+++ backend/src/main/java/com/cdac/carpooling/controller/AuthController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 import com.cdac.carpooling.model.User;
 import com.cdac.carpooling.service.AuthService;
 import com.cdac.carpooling.dto.RegisterRequest;
+import com.cdac.carpooling.dto.ApiResponse;
 import com.cdac.carpooling.dto.LoginRequest;
 
 @RestController
@@ -17,21 +18,26 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> register(@RequestBody RegisterRequest request) {
-        return ResponseEntity.ok(authService.register(
+    public ResponseEntity<ApiResponse<Object>> register(@RequestBody RegisterRequest request) {
+      User user= authService.register(
             request.getName(),
             request.getEmail(),
             request.getPhone(),
             request.getPassword(),
             request.getRoles()
-        ));
+        );
+        return ApiResponse.success(user,"User register successfully.");
     }
 
     @PostMapping("/login")
-    public ResponseEntity<User> login(@RequestBody LoginRequest request) {
-        return ResponseEntity.ok(authService.login(
+    public ResponseEntity<ApiResponse<Object>> login(@RequestBody LoginRequest request) {
+       User user= authService.login(
             request.getEmail(), 
             request.getPassword()
-        ));
+        );
+        if(user==null){
+            return ApiResponse.error("User not found.");
+        }
+        return ApiResponse.success(user,"User loggedin successfully.");
     }
 }
