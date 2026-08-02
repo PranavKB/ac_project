2e101e9afe90610586bde31d45c173b26be588b1
PranavKB <39949849+PranavKB@users.noreply.github.com>
2026-08-02 19:41:36 +0530

    Changing message

--- frontend/src/components/PassengerDashboard.jsx
+++ frontend/src/components/PassengerDashboard.jsx
@@ -606,7 +606,7 @@ function SearchHeaderSection({
           Find a ride in seconds
         </Title>
         <Text type="secondary" style={{ fontSize: "1rem" }}>
-          Verified drivers.
+          Make commuting greener with verified users.
         </Text>
       </div>
       <SearchForm
